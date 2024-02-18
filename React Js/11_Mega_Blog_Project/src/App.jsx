53d443498bce08523bff9779a423ee19bacf3611
bikash-tech-user <bikash.github@gmail.com>
2024-02-18 08:37:28 +0530

    auth service added

---
+++ React Js/11_Mega_Blog_Project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+        <p>Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App
---
+++ React Js/11_Mega_Blog_Project/src/appwrite/auth.js
@@ -0,0 +1,77 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+import { AuthService } from "./auth";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  // to initialize the env propes for appwrite url
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  // make the structure of the functionality to reuse for every service
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      // check if account created then make the user to login else return the userAcoount
+      if (userAccount) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // create login function
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // get current user to show the dashboard
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(
+        "Appwrite Service :: Auth :: getCurrentUser :: error ",
+        error
+      );
+    }
+    //  default value if any problem happens to get the user details
+    return null;
+  }
+
+  // logout
+  async logout() {
+    try {
+      // to delete all the session for the curent user
+      // TODO: refer more the docs for single seeion
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite Service :: Auth :: logout :: error ", error);
+    }
+  }
+}
+
+// create the object
+const authService = new AuthService();
+
+// export object to get all the functions outise of the class
+export default authService;
